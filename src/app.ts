0c09552ef5ad661104168e2f38e4655be00cb702
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-09-16 18:19:02 +0200

    create Drag & Drop interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 //Project Type
 enum ProjectStatus {
   Active,
