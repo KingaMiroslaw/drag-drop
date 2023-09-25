54f4db7c0c2aa756111dba9591015e63a8b16df1
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-09-25 16:05:15 +0200

    organizing files&folders

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,11 @@
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
